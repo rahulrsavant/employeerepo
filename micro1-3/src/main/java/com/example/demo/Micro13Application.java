f1ccd7de2826d8759ea7a74a4de63ae0dac4c7a0
rahulrsavant <50170885+rahulrsavant@users.noreply.github.com>
2020-10-04 11:48:40 +0530

    three four both

---
+++ micro1-3/src/main/java/com/example/demo/Micro13Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Micro13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Micro13Application.class, args);
+	}
+
+}
---
+++ micro1-4/src/main/java/com/example/demo/Micro14Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Micro14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Micro14Application.class, args);
+	}
+
+}
