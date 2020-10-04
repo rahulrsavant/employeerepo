4e63597c215ea8c6d1ae4337e607c9ad3025f056
rahulrsavant <50170885+rahulrsavant@users.noreply.github.com>
2020-10-04 11:51:02 +0530

    micro 5 commit

---
+++ micro1-5/src/main/java/com/example/demo/Micro15Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Micro15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Micro15Application.class, args);
+	}
+
+}
